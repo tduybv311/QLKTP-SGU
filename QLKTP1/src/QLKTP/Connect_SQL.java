07003377695f03b87664d0d770d0ce23ccce5c39
LeHuyTin <118010779+LeHuyTin@users.noreply.github.com>
2022-11-13 00:38:47 +0700

    Add files via upload

---
+++ QLKTP1/src/QLKTP/Connect_SQL.java
@@ -0,0 +1,35 @@
+package QLKTP;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class Connect_SQL {
+    public static Connection getJDBCConnection() throws SQLException {
+        String myDriver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306";
+        String user = "root";
+        String password = "1511";
+         
+        
+        try {
+            Class.forName(myDriver);
+            return DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+        public static void main(String[] args) throws SQLException {
+        Connection connection = getJDBCConnection();
+        
+        if (connection != null) {
+            System.out.println("Data connection successful");
+        } else {
+            System.out.println("Data connection failed");
+        }
+    }
+}
